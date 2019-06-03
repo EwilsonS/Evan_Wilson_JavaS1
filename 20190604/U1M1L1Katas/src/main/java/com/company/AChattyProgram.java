d864f21df49007e0b41020c8bc13c88b90073a29
Evan Loves You <ewilsons@aol.com>
2019-06-03 16:07:56 -0400

    U1L1M1

---
+++ 20190604/U1M1L1Katas/src/main/java/com/company/AChattyProgram.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class AChattyProgram {
+    public static void main(String[] args) {
+
+        String aChattyProgram = "Hi!\n" +
+                "How are you?\n" +
+                "I'm fine!\n" +
+                "Doing really well in fact..\n" +
+                "Isn't it a beautiful day?\n" +
+                "Smiling is my favorite!\n" +
+                "Good bye!";
+
+        System.out.println(aChattyProgram);
+    }
+}
---
+++ 20190604/U1M1L1Katas/src/main/java/com/company/MarvelTable.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class MarvelTable {
+    public static void main(String[] args) {
+        String marvelTable = "|   Hero              |   Affiliation     |   Power         |\n" +
+                "-------------------------------------------------------------\n" +
+                "|   Captain America   |   The Avengers    |   Peak Human    |\n" +
+                "-------------------------------------------------------------\n" +
+                "|   Wolverine         |   The X-Men       |   Healing/Claws |\n" +
+                "-------------------------------------------------------------\n" +
+                "|   Cyclops           |   The X-Men       |   Eye Beam      |\n" +
+                "-------------------------------------------------------------\n" +
+                "|   Domino            |   X-Force         |   Luck          |";
+        System.out.println(marvelTable);
+    }
+}
