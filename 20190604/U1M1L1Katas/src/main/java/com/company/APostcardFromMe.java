8f4b52f761a78ab1b63e78ddb201fe2c04bfb23b
Evan Loves You <ewilsons@aol.com>
2019-06-03 22:40:30 -0400

    added variables

--- 20190604/U1M1L1Katas/src/main/java/com/company/APostcardFromMe.java
+++ 20190604/U1M1L1Katas/src/main/java/com/company/APostcardFromMe.java
@@ -3,30 +3,17 @@ package com.company;
 public class APostcardFromMe {
     public static void main(String[] args) {
 
-//        String aPostCardFromMe = "*------------------------------------------------------------------*\n" +
-//                "| John Smith                                                       |\n" +
-//                "| 123 Main Street                                                  |\n" +
-//                "| Hometown, MN 55555                                               |\n" +
-//                "|                                                                  |\n" +
-//                "|                        Jane Doe                                  |\n" +
-//                "|                        345 Mockingbird Ln.                       |\n" +
-//                "|                        Smalltown, OR 99999                       |\n" +
-//                "|                                                                  |\n" +
-//                "|                                                                  |\n" +
-//                "*------------------------------------------------------------------*";
-//        System.out.println(aPostCardFromMe);
-        System.out.println("*------------------------------------------------------------------*");
-        System.out.println("| John Smith                                                       |");
-        System.out.println("| 123 Main Street                                                  |");
-        System.out.println("| Hometown, MN 55555                                               |");
-        System.out.println("|                                                                  |");
-        System.out.println("|                        Jane Doe                                  |");
-        System.out.println("|                        345 Mockingbird Ln.                       |");
-        System.out.println("|                        Smalltown, OR 99999                       |");
-        System.out.println("|                                                                  |");
-        System.out.println("|                                                                  |");
-        System.out.println("*------------------------------------------------------------------*");
-
-
+        String aPostCardFromMe = "*------------------------------------------------------------------*\n" +
+                "| John Smith                                                       |\n" +
+                "| 123 Main Street                                                  |\n" +
+                "| Hometown, MN 55555                                               |\n" +
+                "|                                                                  |\n" +
+                "|                        Jane Doe                                  |\n" +
+                "|                        345 Mockingbird Ln.                       |\n" +
+                "|                        Smalltown, OR 99999                       |\n" +
+                "|                                                                  |\n" +
+                "|                                                                  |\n" +
+                "*------------------------------------------------------------------*";
+        System.out.println(aPostCardFromMe);
     }
 }
