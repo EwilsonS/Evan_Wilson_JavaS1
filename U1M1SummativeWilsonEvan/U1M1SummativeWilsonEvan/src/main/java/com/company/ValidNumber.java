a5fdbc2f07e83888f945c4c361b256326e668aed
Evan Loves You <ewilsons@aol.com>
2019-06-09 16:44:00 -0400

    summative restructure

--- U1M1SummativeWilsonEvan/U1M1SummativeWilsonEvan/src/main/java/com/company/ValidNumber.java
+++ U1M1SummativeWilsonEvan/U1M1SummativeWilsonEvan/src/main/java/com/company/ValidNumber.java
@@ -11,18 +11,16 @@ public class ValidNumber {
             System.out.println("Enter a number between 1 and 10");
             int num = scan.nextInt();
 
-            while(num < 1 || num > 10) {
+            while (num < 1 || num > 10) {
                 // If the user enters a number less than 1 or greater than 10, display the following error message and prompt the user for another entry:
                 if (num < 1 || num > 10) {
                     System.out.println("You must enter a number between 1 and 10, please try again.");
                     num = scan.nextInt();
                     continue;
-                } else {
-                    // When the user enters a valid number, print the number to the screen and exit.
-                    System.out.println(num);
-                    continue;
                 }
             }
+            // When the user enters a valid number, print the number to the screen and exit.
+            System.out.println(num);
         } catch (java.util.InputMismatchException ex) {
             System.out.println("NUMBERS ONLY! please restart the program.");
 
